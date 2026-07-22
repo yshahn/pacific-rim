6ac72e5238631c4096902ca8487c1a2d94dc90b4
yshahn <yshahn@gmail.com>
2026-07-22 10:06:08 -0400

    Update private-party.js

--- api/private-party.js
+++ api/private-party.js
@@ -27,6 +27,10 @@ async function sendEmail({ to, subject, html }) {
 }
 
 module.exports = async function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  if (req.method === 'OPTIONS') return res.status(200).end();
   if (req.method !== 'POST') return res.status(405).json({ error: 'Method not allowed' });
 
   const {
